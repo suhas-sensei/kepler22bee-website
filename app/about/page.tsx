90583af7270a54f10f7f321c2d1f5df96c90aa64
suhas-sensei <suhas.ghosal2002@gmail.com>
2025-07-23 16:38:27 +0530

    about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,22 +4,19 @@ export default function AboutPage() {
     <>
       <Navbar />
       <main className="min-h-screen bg-black text-white font-['Geist_Mono'] px-15 py-50">
-        <h1 className="text-4xl font-bold mb-4">About Me</h1>
+        <h1 className="text-4xl font-bold mb-4">About us</h1>
         <p className="text-lg max-w-2xl leading-relaxed">
-          Hey! Im the creator of <strong>Kepler22bee</strong>, a digital
-          playground where creativity, code, and curiosity collide. Whether it
-          is building games, crafting digital experiences, or designing
-          expressive web elements — I bring ideas to life with a blend of design
-          and engineering.
+          We make visually rich, fully on-chain games with real world-building.
+          Everything — assets, mechanics, lore — lives on-chain and evolves over
+          time.
         </p>
 
         <div className="mt-6">
           <p className="text-md">
-            ⚡ Interests: Web3, creative coding, interactive design, generative
-            art, AI interfaces.
+            ⚡ Interests: Currently working on a game called BlockRooms.
           </p>
           <p className="text-md mt-2">
-            🛠️ Tools I use: React, Next.js, Tailwind, p5.js, GLSL, and more.
+            🛠️ Tech stack: React, Cairo, Blender, ThreeJS.
           </p>
         </div>
       </main>
